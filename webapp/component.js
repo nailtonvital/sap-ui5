0a4bd67ff840ccb84dee8abf9752ccbb91dae165
Nailton Vital <nailtonvital35@gmail.com>
2022-12-16 19:34:01 -0300

    .
    Descriptor for Applications 
    
    Pages and Panels 
    
    Shell Control as Container
    
    Margins and Paddings
    
    Custom CSS and Theme Colors
    
    Nested Views

--- webapp/component.js
+++ webapp/component.js
@@ -8,17 +8,12 @@ sap.ui.define(
     "use strict";
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
       metadata: {
-        rootView: {
-          "ViewName": "sap.ui.demo.walkthrough.view.App",
-          "type": "XML",
-          "async": true,
-          "id": "app",
-        },
+        manifest: "json",
       },
       init: function () {
-        //call the init function of the parent
+        // call the init function of the parent
         UIComponent.prototype.init.apply(this, arguments);
-        //set data models
+        // set data models
         var oData = {
           recipient: {
             name: "UI5",
@@ -26,14 +21,6 @@ sap.ui.define(
         };
         var oModel = new JSONModel(oData);
         this.setModel(oModel);
-
-        //set i18n model
-        var i18nModel = new ResourceModel({
-          bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
-          supportedLocales: [""],
-          fallbackLocale: "",
-        });
-        this.setModel(i18nModel, "i18n");
       },
     });
   }
